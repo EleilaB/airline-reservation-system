a976649318b53f5d8d09d9e37a16aaac95990ec1
Eleila Bradford <eleila.bradford@gmail.com>
2022-12-01 22:19:52 -0700

    Part 2

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args){
-        Person person = new Person();
+        Person person = new Person("Eleila Bradord", "American", "Aug 17 1999", 5);
 
-        person.name = "Eleila Bradford";
-        person.nationality = "American";
-        person.dateOfBirth = "Aug 17 1999";
-        person.passport = new String[]{person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 5;
+        System.out.println("Name: " + person.name + "\nNationality: " + person.nationality + "\nDate of Birth: " + person.dateOfBirth + "\nSeat Number: " + person.seatNumber);
 
-        System.out.println(person.name);
-        System.out.println(person.nationality);
-        System.out.println(person.dateOfBirth);
-        System.out.println(Arrays.toString(person.passport));
-        System.out.println(person.seatNumber);
     }
 
 }
--- src/Person.java
+++ src/Person.java
@@ -6,4 +6,11 @@ public class Person {
     String[] passport;
     int seatNumber;
 
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber){
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
 }
